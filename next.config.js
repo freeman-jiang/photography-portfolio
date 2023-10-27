75dd8f1ae85d15f021bb042b471a29b8a2b9c17e
Freeman Jiang <freeman.jiang.ca@gmail.com>
2023-10-27 00:33:52 -0400

    add contentful codegen

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  transpilePackages: ["contentful-typescript-codegen"],
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
