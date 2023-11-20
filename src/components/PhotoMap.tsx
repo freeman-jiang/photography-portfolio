ffc86285f5e19d12bf02ac217aa96a68e8fcaecd
Freeman Jiang <freeman.jiang.ca@gmail.com>
2023-11-20 16:02:24 -0500

    working popups

--- src/components/PhotoMap.tsx
+++ src/components/PhotoMap.tsx
@@ -2,33 +2,43 @@
 import { IPhoto, IPhotoFields } from "@/types/generated/contentful";
 import "mapbox-gl/dist/mapbox-gl.css";
 import { useState } from "react";
+import { Marker, Popup } from "react-map-gl";
 
-import Map, { Marker } from "react-map-gl";
+import Map from "react-map-gl";
 
 interface Props {
   accessToken: string;
   photos: IPhoto[];
 }
 
+interface MarkerProps {
+  index: number;
+  photo: IPhoto;
+}
+
+interface PopupProps {
+  index: number;
+  photo: IPhoto;
+}
+
 export const PhotoMap = ({ accessToken, photos }: Props) => {
-  console.log(photos);
-  const [popupIndex, setPopupIndex] = useState<number | null>(null);
+  const photosById = Object.fromEntries(
+    photos.map((photo) => [photo.sys.id, photo])
+  );
 
-  const openPopup = (index: number) => {
-    setPopupIndex(index);
-  };
+  const [openPopups, setOpenPopups] = useState<{ [key: string]: boolean }>({}); // photo id -> boolean
 
-  const closePopup = () => {
-    setPopupIndex(null);
-  };
-
-  const CustomMarker = ({ index, item }: { index: number; item: IPhoto }) => {
-    const fields = item.fields as IPhotoFields;
+  const CustomMarker = ({ index, photo }: MarkerProps) => {
+    const fields = photo.fields as IPhotoFields;
+    const photoId = photo.sys.id;
 
     return (
       <Marker
         longitude={fields.coordinates.lon}
         latitude={fields.coordinates.lat}
+        onClick={() => {
+          setOpenPopups((prev) => ({ ...prev, [photoId]: true }));
+        }}
       >
         {/* <div className="marker" onClick={() => openPopup(index)}>
           <div className="text-xl font-bold text-emerald-500">HELLO WORLD</div>
@@ -36,6 +46,34 @@ export const PhotoMap = ({ accessToken, photos }: Props) => {
       </Marker>
     );
   };
+
+  const CustomPopup = ({ index, photo }: PopupProps) => {
+    const photoId = photo.sys.id;
+    if (!openPopups[photoId]) return null;
+
+    const fields = photo.fields as IPhotoFields;
+
+    return (
+      <Popup
+        longitude={fields.coordinates.lon}
+        latitude={fields.coordinates.lat}
+        closeButton={true}
+        onClose={() => {
+          setOpenPopups((prev) => ({ ...prev, [photoId]: false }));
+        }}
+        closeOnClick={false}
+      >
+        You are here
+      </Popup>
+    );
+  };
+
+  const markers = Object.entries(photosById).map(([id, photo], index) => (
+    <div key={id}>
+      <CustomMarker index={index} photo={photo} />
+      <CustomPopup index={index} photo={photo} />
+    </div>
+  ));
 
   return (
     <div className="flex justify-center">
@@ -52,9 +90,7 @@ export const PhotoMap = ({ accessToken, photos }: Props) => {
           name: "globe",
         }}
       >
-        {photos.map((item, index) => (
-          <CustomMarker key={index} index={index} item={item} />
-        ))}
+        {markers}
       </Map>
     </div>
   );
