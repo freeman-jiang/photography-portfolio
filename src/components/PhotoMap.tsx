4bbf16302adc9e45d9a2e3321e44d6c72c312e70
Freeman Jiang <freeman.jiang.ca@gmail.com>
2023-11-20 16:47:48 -0500

    fix popup flash and simplify

--- src/components/PhotoMap.tsx
+++ src/components/PhotoMap.tsx
@@ -1,80 +1,16 @@
 "use client";
-import { IPhoto, IPhotoFields } from "@/types/generated/contentful";
+import { IPhoto } from "@/types/generated/contentful";
 import "mapbox-gl/dist/mapbox-gl.css";
-import { useState } from "react";
-import { Marker, Popup } from "react-map-gl";
 
 import Map from "react-map-gl";
+import { PhotoMarker } from "./PhotoMarker";
 
 interface Props {
   accessToken: string;
   photos: IPhoto[];
 }
 
-interface MarkerProps {
-  index: number;
-  photo: IPhoto;
-}
-
-interface PopupProps {
-  index: number;
-  photo: IPhoto;
-}
-
 export const PhotoMap = ({ accessToken, photos }: Props) => {
-  const photosById = Object.fromEntries(
-    photos.map((photo) => [photo.sys.id, photo])
-  );
-
-  const [openPopups, setOpenPopups] = useState<{ [key: string]: boolean }>({}); // photo id -> boolean
-
-  const CustomMarker = ({ index, photo }: MarkerProps) => {
-    const fields = photo.fields as IPhotoFields;
-    const photoId = photo.sys.id;
-
-    return (
-      <Marker
-        longitude={fields.coordinates.lon}
-        latitude={fields.coordinates.lat}
-        onClick={() => {
-          setOpenPopups((prev) => ({ ...prev, [photoId]: true }));
-        }}
-      >
-        {/* <div className="marker" onClick={() => openPopup(index)}>
-          <div className="text-xl font-bold text-emerald-500">HELLO WORLD</div>
-        </div> */}
-      </Marker>
-    );
-  };
-
-  const CustomPopup = ({ index, photo }: PopupProps) => {
-    const photoId = photo.sys.id;
-    if (!openPopups[photoId]) return null;
-
-    const fields = photo.fields as IPhotoFields;
-
-    return (
-      <Popup
-        longitude={fields.coordinates.lon}
-        latitude={fields.coordinates.lat}
-        closeButton={true}
-        onClose={() => {
-          setOpenPopups((prev) => ({ ...prev, [photoId]: false }));
-        }}
-        closeOnClick={false}
-      >
-        You are here
-      </Popup>
-    );
-  };
-
-  const markers = Object.entries(photosById).map(([id, photo], index) => (
-    <div key={id}>
-      <CustomMarker index={index} photo={photo} />
-      <CustomPopup index={index} photo={photo} />
-    </div>
-  ));
-
   return (
     <div className="flex justify-center">
       <Map
@@ -90,7 +26,9 @@ export const PhotoMap = ({ accessToken, photos }: Props) => {
           name: "globe",
         }}
       >
-        {markers}
+        {photos.map((photo) => (
+          <PhotoMarker key={photo.sys.id} photo={photo} />
+        ))}
       </Map>
     </div>
   );
---
+++ src/components/PhotoMarker.tsx
@@ -0,0 +1,55 @@
+import { IPhoto, IPhotoFields } from "@/types/generated/contentful";
+import { X } from "lucide-react";
+import { useState } from "react";
+import { Marker, Popup } from "react-map-gl";
+import { Button } from "./ui/button";
+
+interface Props {
+  photo: IPhoto;
+}
+
+interface CustomPopupProps {
+  photo: IPhoto;
+  toggle: () => void;
+}
+
+const CustomPopup = ({ photo, toggle }: CustomPopupProps) => {
+  const fields = photo.fields as IPhotoFields;
+
+  return (
+    <Popup
+      longitude={fields.coordinates.lon}
+      latitude={fields.coordinates.lat}
+      closeButton={false}
+      closeOnClick={false}
+    >
+      <div className="">
+        <div className="flex justify-between items-center">
+          <div className="mr-3 font-sans text-sm">{fields.location}</div>
+          <Button onClick={toggle} className="p-0 h-5 w-5">
+            <X className="h-2.5 w-2.5" />
+          </Button>
+        </div>
+      </div>
+    </Popup>
+  );
+};
+
+export const PhotoMarker = ({ photo }: Props) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const fields = photo.fields as IPhotoFields;
+
+  const toggle = () => setIsOpen((prev) => !prev);
+
+  return (
+    <Marker
+      longitude={fields.coordinates.lon}
+      latitude={fields.coordinates.lat}
+      onClick={() => {
+        toggle();
+      }}
+    >
+      {isOpen && <CustomPopup photo={photo} toggle={toggle} />}
+    </Marker>
+  );
+};
