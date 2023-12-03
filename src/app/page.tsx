027dc94915b7ba46f8db6bc7846b36cd23a9eaa0
Freeman Jiang <freeman.jiang.ca@gmail.com>
2023-12-02 22:23:22 -0500

    unveil map

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { Gallery } from "@/components/Gallery";
 import { Button } from "@/components/ui/button";
+import { Navigation } from "lucide-react";
 import Link from "next/link";
 
 export const revalidate = 10;
@@ -9,11 +10,10 @@ export default function Home() {
     <main className="py-8">
       <div className="px-4 font-thin tracking-wide text-3xl lg:px-10 sm:px-6 text-center sm:text-left flex justify-between">
         <div className="font-serif">Freeman Jiang</div>
-        <Link
-          href="https://uwaterloo.ca/co-operative-education/work-abroad/photo-contest/vote#Photo-05"
-          target="_blank"
-        >
-          <Button>Vote for me!</Button>
+        <Link href="/map">
+          <Button>
+            <Navigation className="w-4 h-4 mr-2" /> Map
+          </Button>
         </Link>
       </div>
       <Gallery />
