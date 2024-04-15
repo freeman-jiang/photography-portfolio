4cdf8d07cf28b4d626bf64ca5ddca17ef003b727
Freeman Jiang <freeman.jiang.ca@gmail.com>
2024-04-15 00:26:20 -0400

    proper theme button animation

--- src/components/ThemeButton.tsx
+++ src/components/ThemeButton.tsx
@@ -15,8 +15,8 @@ export const ThemeButton = () => {
       }}
       className="p-2 size-10 dark:bg-stone-800"
     >
-      <Sun className="rotate-0 scale-100 dark:-rotate-90 dark:scale-0" />
-      <MoonStar className="absolute rotate-90 scale-0 dark:rotate-0 dark:scale-100" />
+      <Sun className="rotate-0 scale-100 dark:-rotate-90 dark:scale-0 transition-transform duration-700" />
+      <MoonStar className="absolute rotate-90 scale-0 dark:rotate-0 dark:scale-100 transition-transform duration-700" />
       <span className="sr-only">Toggle theme</span>
     </Button>
   );
