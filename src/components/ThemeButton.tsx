506bc7fdcd42de2012ce5e24128a3439bca9ac31
Freeman Jiang <freeman.jiang.ca@gmail.com>
2024-04-15 13:25:40 -0400

    invert colors

--- src/components/ThemeButton.tsx
+++ src/components/ThemeButton.tsx
@@ -22,7 +22,7 @@ export const ThemeButton = () => {
         onClick={() => {
           setTheme(theme === "dark" ? "light" : "dark");
         }}
-        className="p-2 hover:bg-inherit size-12 dark:bg-stone-800 shadow-lg dark:shadow-yellow-300/50 dark:hover:shadow-yellow-300/90 shadow-blue-500/60 hover:shadow-blue-500/90 transition-shadow duration-300"
+        className="p-2 hover:bg-inherit size-12 dark:bg-stone-800 shadow-lg shadow-yellow-300/50 hover:shadow-yellow-300/90 dark:shadow-blue-500/65 dark:hover:shadow-blue-500/90 transition-shadow duration-300 hover:bg-stone-50"
       >
         <Sun className="rotate-0 scale-100 dark:-rotate-90 dark:scale-0 transition-transform duration-700" />
         <MoonStar className="absolute rotate-90 scale-0 dark:rotate-0 dark:scale-100 transition-transform duration-700" />
